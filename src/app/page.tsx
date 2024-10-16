9cb1d48b78439c81d9c17cee2b3123dec25e2e24
pablitomartinez <86635786+pablitomartinez@users.noreply.github.com>
2024-10-16 15:58:48 -0300

    resolviendo conflictos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,9 @@ import Navbar from "@/components/navbar";
 import AboutMe from "./aboutMe";
 // import Projects from "./Projects";
 import Footer from "@/components/footer";
-<<<<<<< HEAD
 // import Skills from "./Skills";
 import FormularioContacto from "@/components/formContac";
 // import Projects from "./Projects";
-=======
-import Skills from "./Skills";
-import FormularioContacto from "@/components/formContac";
-
->>>>>>> 28668fb (feat: implementar configuración global de TailwindCSS y aplicar estilos consistentes en todos los componentes)
 
 export default function Home() {
   return (
@@ -19,13 +13,8 @@ export default function Home() {
       <Navbar />
       <Hero />
       <AboutMe />
-<<<<<<< HEAD
       {/* <Skills /> */}
       {/* <Projects /> */}
-=======
-      <Skills />
-      <Projects />
->>>>>>> 28668fb (feat: implementar configuración global de TailwindCSS y aplicar estilos consistentes en todos los componentes)
       <FormularioContacto />
       <Footer />
     </div>

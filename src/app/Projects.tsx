7039ca4ee64efd785f1667be32c88498b0a87dd1
pablitomartinez <86635786+pablitomartinez@users.noreply.github.com>
2024-10-22 12:31:20 -0300

    feat: se creo componente de proyectos, tambien se agrego una carpeta para modularizar y mantener limpio el codigo

--- src/app/Projects.tsx
+++ src/app/Projects.tsx
@@ -1,27 +1,60 @@
-// components/Projects.tsx
+import { projectsData } from "../data/projectsData"; // Importa los datos de los proyectos
+import Image from "next/image"; // Importa el componente Image de Next.js
 
 const Projects = () => {
   return (
-    <section id="projects" className="py-16 bg-primary text-text">
-      <div className="container mx-auto px-6">
-        <h2 className="text-3xl font-bold text-center mb-8">Projects</h2>
-        <div className= "grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          <div className="bg-white p-6 shadow-lg">
-            <h3 className="text-xl font-semibold mb-2">Project 1</h3>
-            <p className="text-gray-600">Description of Project 1.</p>
+    <section className="text-gray-400 bg-primary body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-col">
+          <div className="h-1 bg-gray-800 rounded overflow-hidden">
+            <div className="w-24 h-full bg-accent"></div>
           </div>
-          <div className="bg-background p-6 shadow-custom-dark rounded-lg">
-            <h3 className="text-xl font-sans font-semibold mb-2 text-secondary">
-              Project 2
-            </h3>
-            <p className="text-text">Description of Project 2.</p>
+          <div className="flex flex-wrap sm:flex-row flex-col py-6 mb-12">
+            <h1 className="sm:w-full text-text font-heading text-3xl mb-2 sm:mb-0 text-center">
+              Mis Proyectos
+            </h1>
           </div>
-          <div className="bg-background p-6 shadow-custom-dark rounded-lg">
-            <h3 className="text-xl font-sans font-semibold mb-2 text-secondary">
-              Project 3
-            </h3>
-            <p className="text-text">Description of Project 3.</p>
-          </div>
+        </div>
+
+        {/* Renderizar proyectos utilizando el array importado */}
+        <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
+          {projectsData.map((project, index) => (
+            <div key={index} className="p-4 md:w-1/3 sm:mb-0 mb-6">
+              <div className="rounded-lg h-64 overflow-hidden shadow-lg hover:shadow-2xl transition duration-300 relative">
+                {/* Utilizar el componente Image en lugar de img */}
+                {/* <Image
+                  alt={project.title}
+                  src={project.image}
+                  layout="fill"
+                  objectFit="cover"
+                  quality={75} // Puedes ajustar la calidad si lo deseas
+                /> */}
+              </div>
+              <h2 className="text-xl font-heading text-text mt-5">
+                {project.title}
+              </h2>
+              <p className="text-base leading-relaxed mt-2 text-secondary">
+                {project.description}
+              </p>
+              <a
+                href={project.link}
+                className="text-accent inline-flex items-center mt-3 hover:text-accent-dark transition duration-300"
+              >
+                Ver Proyecto
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  className="w-4 h-4 ml-2"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M5 12h14M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+            </div>
+          ))}
         </div>
       </div>
     </section>
---
+++ src/data/projectsData.ts
@@ -0,0 +1,23 @@
+export const projectsData = [
+  {
+    title: "Shooting Stars",
+    description:
+      "Swag shoindxgoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.",
+    image: "https://dummyimage.com/1203x503",
+    link: "/proyecto1",
+  },
+  {
+    title: "The Catalyzer",
+    description:
+      "Swag shoindxigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.",
+    image: "https://dummyimage.com/1204x504",
+    link: "/proyecto2",
+  },
+  {
+    title: "The 400 Blows",
+    description:
+      "Swag shoindegoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.",
+    image: "https://dummyimage.com/1205x505",
+    link: "/proyecto3",
+  },
+];

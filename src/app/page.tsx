fc7fe0dde9ed0dc24fd09fc44b31e1b861df113e
pablitomartinez <86635786+pablitomartinez@users.noreply.github.com>
2024-10-23 11:04:28 -0300

    feat: se agrego texto a footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,9 +31,9 @@ export default function Home() {
         <div id="contact">
           <FormularioContacto />
         </div>
+      </div>
         {/* Footer */}
         <Footer />
-      </div>
     </>
   );
 }
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,30 +1,49 @@
-// components/Footer.tsx
+import { FaLinkedin, FaGithub, FaTwitter, FaInstagram } from "react-icons/fa"; // Importa íconos de redes sociales
+
 const Footer = () => {
   return (
     <footer className="bg-primary text-text py-10">
       <div className="container mx-auto px-6 text-center">
         <p className="text-lg md:text-xl text-accent mb-6 font-heading">
-          Hey Folks 🔥
+          ¡Gracias por visitarme! 👋
         </p>
         <p className="text-md md:text-lg text-text mb-6 font-sans">
-          I hope you enjoy my portfolio as much as I enjoyed creating it. Feel
-          free to show your ❤️
+          Espero que te haya gustado mi portfolio tanto como a mí me encantó
+          crearlo. Si te interesa trabajar conmigo, no dudes en contactarme.
         </p>
         <p className="text-md md:text-lg text-text font-sans">
-          I am available for new opportunities, drop your business inquiry to:
+          Estoy disponible para nuevas oportunidades. ¡Envíame tu propuesta!
           <a
             href="mailto:altiorasoftware@gmail.com"
             className="text-secondary underline"
           >
             {" "}
-            altiorasoftware@gmail.com
+          pablitoemartinez666@gmail.com
           </a>
         </p>
+
+        {/* Redes sociales */}
+        <div className="mt-8 flex justify-center space-x-6">
+          <a href="https://linkedin.com" target="_blank" aria-label="LinkedIn">
+            <FaLinkedin size={24} className="text-text hover:text-accent" />
+          </a>
+          <a href="https://github.com" target="_blank" aria-label="GitHub">
+            <FaGithub size={24} className="text-text hover:text-accent" />
+          </a>
+          <a href="https://twitter.com" target="_blank" aria-label="Twitter">
+            <FaTwitter size={24} className="text-text hover:text-accent" />
+          </a>
+          <a
+            href="https://instagram.com"
+            target="_blank"
+            aria-label="Instagram"
+          >
+            <FaInstagram size={24} className="text-text hover:text-accent" />
+          </a>
+        </div>
+
         <div className="mt-8">
-          <p className="text-text font-sans">Developed by Pablo</p>
-          <p className="text-sm text-gray-500 font-sans">
-            © {new Date().getFullYear()} Altiora. All rights reserved.
-          </p>
+          <p className="text-text font-sans">Desarrollado por Pablo Martínez 💜</p>
         </div>
       </div>
     </footer>
